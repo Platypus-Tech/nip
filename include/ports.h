a1b8bac6073f127ed5af0611dc22ff8395ad8a0b
GNUOverYooouu <dtortoise99@gmail.com>
2021-04-05 14:41:30 -0700

    Add file with ports

---
+++ include/ports.h
@@ -0,0 +1,14 @@
+/********************************************************************************************
+ * Defining ports                                                                           *
+ * (c) DamieFC 2021                                                                         *
+ ********************************************************************************************/
+#define HTTP_PORT_1 1
+#define HTTPS_PORT_2 2
+#define FTP_PORT_3 3
+#define SSH_PORT_4 4
+#define TELNET_PORT_5 5
+#define SMTP_PORT_6 6
+#define DNS_PORT_7 7
+#define POP3_PORT_8
+#define SEC_ME_PORT_9 9
+#define IRC_PORT_10
